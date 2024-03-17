a884992be0abefdbe7d8bb4ab05ad3397a36e53a
damaroja <ildefonsoarrecife3@hotmail.com>
2024-03-17 14:26:00 +0000

    Create 11FunctionDeclaration.js

---
+++ IniciacionJs/11FunctionDeclaration.js
@@ -0,0 +1,9 @@
+
+
+//Functions in js
+
+//Funtcion declaration--------------
+//Una function declaration es una funcion que se declara con la palabra reservada function
+function greet(){
+    console.log('Hello');
+}
