2115d7d2fac2e28d74ebf3a77be440d593716174
damaroja <ildefonsoarrecife3@hotmail.com>
2024-03-17 14:43:41 +0000

    Create 13Igualdad===.==.js

---
+++ IniciacionJs/13Igualdad===.==.js
@@ -0,0 +1,11 @@
+//Igualdad estricta e igualdad no estricta
+//Igualdad estricta
+console.log(2 === 2); //true
+
+//Igualdad no estricta
+
+console.log(2 == 2); //true
+
+//Diferencia estricta y no estricta
+//La diferencia estricta es con tres signos de igualdad y la no estricta con dos signos de igualdad. 
+//En las diferencias estrictas se compara el valor y el tipo de dato, en las no estrictas solo se compara el valor.
