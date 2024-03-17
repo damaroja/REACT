37d06d76330994b1a9d1ca79cc463723ff41af39
damaroja <ildefonsoarrecife3@hotmail.com>
2024-03-17 13:31:29 +0000

    Create 01TiposDeDatos.js

---
+++ IniciacionJs/01TiposDeDatos.js
@@ -0,0 +1,61 @@
+
+
+//Tipos de datos en js
+
+//String
+let nombre = "Juan";
+console.log(nombre);
+console.log(typeof nombre);
+
+//Number
+let numero = 1000;
+console.log(numero);
+console.log(typeof numero);
+
+//Boolean
+let boolean = true;
+console.log(boolean);
+console.log(typeof boolean);
+
+//Null
+let nullVariable = null;
+console.log(nullVariable);
+console.log(typeof nullVariable);
+
+//Undefined
+let undefinedVariable;
+console.log(undefinedVariable);
+console.log(typeof undefinedVariable);
+
+//Array
+let array = [1,2,3,4,5];
+console.log(array);
+console.log(typeof array);
+
+//Object
+let object = {
+    nombre: "Juan",
+    apellido: "Perez"
+}
+console.log(object);
+console.log(typeof object);
+
+//Function
+function miFuncion(){
+    console.log("Hola");
+}
+console.log(miFuncion);
+
+//bIGINT
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
+console.log(typeof bigInt);
+
+//Symbol
+const simbolo = Symbol("simbolo");
+console.log(simbolo);
+console.log(typeof simbolo);
+
+
+
+
