776a8c0fe03a3a58e7b3070aa5563e8c3a9c62b9
damaroja <ildefonsoarrecife3@hotmail.com>
2024-03-18 11:09:32 +0000

    Create 19DOMSeleccion.js

---
+++ IniciacionJs/19DOMSeleccion.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+//DOM selecionar elementos con querySelector. Devuelve el primer elemento que cumpla con la condición
+//clases
+const header = document.querySelector('.header');
+//id
+const contenedor = document.querySelector('#contenedor');
+//etiquetas
+const h2 = document.querySelector('h2');
+
+//DOM seleccionar elementos con querySelectorAll. Devuelve un array de elementos
+//clases
+const enlaces = document.querySelectorAll('.enlace');
+//etiquetas
+const imagenes = document.querySelectorAll('img');
+//id
+const contenedor2 = document.querySelectorAll('#contenedor');
